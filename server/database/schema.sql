94f18ef4689ed962b97f80b37a1dc008cdff2fb3
Mkathalina Sanchez <mkathalina.sanchez@gmail.com>
2024-07-09 16:39:09 +0200

    add users and cities test in database.

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,2 +1,7 @@
-INSERT INTO city,
-WHERE
+INSERT INTO city (name) VALUES ('paris');
+INSERT INTO user (pseudo, email, password, is_admin) VALUES ('john_doe', 'johndoe@yopmail.com', '1234', 1);
+INSERT INTO user (pseudo, email, password, is_admin) VALUES ('jean_french', 'jeanfrench@yopmail.com', '4567', 0);
+INSERT INTO user (pseudo, email, password, is_admin) VALUES ('juana_latina', 'juanalatina@yopmail.com', '8910', 0);
+INSERT INTO city (name) VALUES ('lisbon');
+INSERT INTO city (name) VALUES ('madrid');
+
