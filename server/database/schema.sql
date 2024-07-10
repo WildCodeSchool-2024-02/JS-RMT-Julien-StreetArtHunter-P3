f597e52b5e1fc563da93f805c4816ba1b0c540f2
Johnny Domingues <johnnydomingues@sfr.fr>
2024-07-10 10:53:57 +0200

    récupération de la db via le schéma sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -57,9 +57,8 @@ CREATE TABLE seen (
 );
 
 INSERT INTO city (name) VALUES ('paris');
+INSERT INTO city (name) VALUES ('lisbon');
+INSERT INTO city (name) VALUES ('madrid');
 INSERT INTO user (pseudo, email, password, is_admin) VALUES ('john_doe', 'johndoe@yopmail.com', '1234', 1);
 INSERT INTO user (pseudo, email, password, is_admin) VALUES ('jean_french', 'jeanfrench@yopmail.com', '4567', 0);
 INSERT INTO user (pseudo, email, password, is_admin) VALUES ('juana_latina', 'juanalatina@yopmail.com', '8910', 0);
-INSERT INTO city (name) VALUES ('lisbon');
-INSERT INTO city (name) VALUES ('madrid');
-
