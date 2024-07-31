be3768fa103ba7de54e10ed7e57ecb607d97c907
Vanessa <nessa_g@hotmail.fr>
2024-07-31 10:06:31 +0200

    fix css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,25 @@
-import { Link } from 'react-router-dom';
-import '../styles/header.css';
-import '../styles/button-login.css';
+import { Link } from "react-router-dom";
+import "../styles/header.css";
+import "../styles/button-login.css";
 import logo from "../assets/logo.png";
 
 function Header() {
   return (
     <header className="header">
       <Link to="/home" className="logo">
-        <img src= {logo} alt="logo" className="logo"/>
+        <img src={logo} alt="logo" className="logo" />
       </Link>
       <nav className="navigation">
-        <Link to="/gallery" className="button.login">Galerie</Link>
-        <Link to="/login" className="button.login">Connexion</Link>
+        <Link to="/gallery">
+          <button type="button" className="button-login">
+          Galerie
+          </button>
+        </Link>
+        <Link to="/login">
+          <button type="button" className="button-login">
+          Connexion
+          </button>
+        </Link>
       </nav>
     </header>
   );
