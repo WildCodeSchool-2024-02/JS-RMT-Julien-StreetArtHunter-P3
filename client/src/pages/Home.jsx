3a83c49b2e1e8ebbae5f7caa7dfe223f6eb04d53
Vanessa <nessa_g@hotmail.fr>
2024-07-31 16:28:26 +0200

    Hotfix merge

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,9 @@
 import Geolocation from "../components/Geolocation";
-import Header from "../components/Header";
 import "../styles/Geolocation.css";
 
 function Home() {
   return (
-    <div>
-      <Header />
       <Geolocation />
-    </div>
   );
 }
 
