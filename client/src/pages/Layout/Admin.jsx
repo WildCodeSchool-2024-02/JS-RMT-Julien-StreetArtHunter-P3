69fa91714e800ea7f122f0f8c97cc7acfb54bd09
Johnny Domingues <johnnydomingues@sfr.fr>
2024-08-06 15:42:19 +0200

    rename files bis

--- client/src/pages/Layout/Admin.jsx
+++ client/src/pages/Layout/Admin.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavAdmin from "../../components/NavAdmin";
-import "../../styles/styles-pages/admin.css";
+import "../../styles/styles-pages/admin-interface.css";
 
 function Admin() {
   return (
