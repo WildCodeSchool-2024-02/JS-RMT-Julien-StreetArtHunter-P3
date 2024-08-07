49855eb27f41466508689c7c0a6258361d9ea053
Megane <megane.loisele@gmail.com>
2024-08-07 16:22:52 +0200

    amelioration du css de la page d'accueil

--- client/src/components/HomeEnter.jsx
+++ client/src/components/HomeEnter.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import citybackground from "../assets/city-background.png";
 import logo from "../assets/logo.png";
 import "../App.css";
@@ -5,17 +6,23 @@ import "../styles/home-enter.css";
 import "../styles/button.css";
 
 function HomeEnter() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className="App-header-enter">
       <img src={citybackground} className="App-background" alt="background" />
       <div className="content">
         <img src={logo} className="title-image" alt="Street Art Hunter" />
-        <button type="button" className="button">
+        <button type="button" className="button" onClick={handleClick}>
           Entrer
         </button>
       </div>
       <div className="about-section">
-        <h2>About us</h2>
+        <h2>A Propos</h2>
         <p>
           Nous préparons un prototype d’appli web pour tous les amateurs de
           street-art en ville. Notre appli promet une expérience visuelle
