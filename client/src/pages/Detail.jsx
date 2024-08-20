0f08b1252e9b5b49935b20dba9a1e06450bd5af5
Vanessa <nessa_g@hotmail.fr>
2024-08-20 16:52:45 +0200

    Fin de la mise en style de la page detail

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,32 +1,35 @@
-import { useLoaderData} from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import Header from "../components/Header";
-import Carousel from "../components/Carousel"; 
 import "../styles/detail.css";
 
 function Detail() {
-  const streetArt = useLoaderData(); 
-  
+  const streetArt = useLoaderData();
+
   if (!streetArt) {
     return <p>StreetArt non trouvé</p>;
   }
   return (
     <div>
       <Header />
-      <section className="detail-streetart-container">
-        <h2 className="streetart-title">{streetArt.title}</h2>
-        <p className="streetart-artist">Artiste: {streetArt.name}</p>
-        <div className="streetart-image-container">
-          <img
-            src={`${import.meta.env.VITE_API_URL}/${streetArt.image_url}`}
-            alt={streetArt.image_alt}
-            className="streetart-image"
-          />
+      <section className="container">
+        <img
+          className="streetart-image"
+          src={`${import.meta.env.VITE_API_URL}/${streetArt.image_url}`}
+          alt={streetArt.image_alt}
+        />
+        <div className="detail-streetart-container">
+          <h2>{streetArt.title}</h2>
+          <h3>{streetArt.category_title}</h3>
+          <h4>{streetArt.city_name}</h4>
+          <p> Artiste: {streetArt.artist_name}</p>
+          <div>
+            <h2>Description:</h2>
+            {streetArt.description && (
+              <p className="streetart-description">{streetArt.description}</p>
+            )}
+          </div>
         </div>
-        {streetArt.description && (
-          <p className="streetart-description">{streetArt.description}</p>
-        )}
       </section>
-      <Carousel />
     </div>
   );
 }
