ecd0d323c01bbedc58a0fcc46bbae20e614f3e68
Vanessa <nessa_g@hotmail.fr>
2024-08-21 10:08:52 +0200

    MAJ Database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -30,7 +30,7 @@ CREATE TABLE category (
 CREATE TABLE streetart (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    description TEXT,
+    description LONGTEXT NOT NULL,
     geolocation_x DECIMAL(8, 6) NOT NULL,
     geolocation_y DECIMAL(9, 6) NOT NULL,
     image_url VARCHAR(255) NOT NULL,
@@ -237,7 +237,7 @@ INSERT INTO
     )
 VALUES (
         'Chuuuttt',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.'
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.',
         'assets/images/Chuuuttt.jpg',
         1,
         5,
@@ -257,7 +257,7 @@ INSERT INTO
     )
 VALUES (
         'Amor com as Mulheres',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.'
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.',
         'assets/images/amor-com-as-mulheres.jpg',
         3,
         7,
@@ -277,7 +277,7 @@ INSERT INTO
     )
 VALUES (
         'The cat',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.'
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.',
         'assets/images/the-cat.jpg',
         1,
         4,
@@ -297,7 +297,7 @@ INSERT INTO
     )
 VALUES (
         'Champagne Rat',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.'
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.',
         'assets/images/rat-champagne.jpg',
         1,
         1,
@@ -317,7 +317,7 @@ INSERT INTO
     )
 VALUES (
         'Petite fille, espoir',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.'
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae ipsum non odio scelerisque vestibulum. Aliquam auctor mauris quis vestibulum hendrerit. Aliquam nec sapien id quam porta varius at vitae mauris. Ut elementum velit vel nulla mollis, et tincidunt eros tempus. Praesent finibus nunc sit amet ligula pharetra mattis. Aliquam erat volutpat. In a elit ac justo sodales vulputate. Aenean eget arcu a elit facilisis blandit quis at augue. Aenean fringilla urna vitae eleifend iaculis. Nullam vulputate tellus ut mattis ornare. Mauris at nunc aliquam, auctor ipsum sodales, ullamcorper dolor. Aenean iaculis sit amet purus ac condimentum. Sed eu luctus odio, sed mollis elit. Sed libero velit, varius nec tortor non, pellentesque facilisis leo. Morbi eget massa erat.',
         'assets/images/petite-fille-espoir.jpg',
         4,
         5,
