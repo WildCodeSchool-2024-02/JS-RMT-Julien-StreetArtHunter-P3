52fd8afbef7d0a7c05d066eb753cf73a216447b7
Mkathalina Sanchez <mkathalina.sanchez@gmail.com>
2024-08-27 16:18:56 +0200

    test button.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 import "../styles/header.css";
 import "../styles/button.css";
 import logo from "../assets/logo.png";
+
 
 function Header() {
   return (
@@ -16,6 +17,12 @@ function Header() {
         <Link to="/login">
           <p className="button">Connexion</p>
         </Link>
+        <Link to="/register">
+          <p className="button">Inscription</p>
+        </Link>
+       <div>
+          <p className="button">test</p>
+        </div>
       </nav>
     </header>
   );
