c255bce0a98d208f310e52afc9c344f51280239e
Mkathalina Sanchez <mkathalina.sanchez@gmail.com>
2024-08-27 17:38:47 +0200

    update navigation bar and user status.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,40 @@
-import { Link} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/header.css";
 import "../styles/button.css";
 import logo from "../assets/logo.png";
+import { useLogin } from "../context/LoginContext";
 
 
 function Header() {
+  // Récupère les infos du context
+  const { user, setUser } = useLogin();
+  const navigate = useNavigate();
+
+  // Fonction handleLogout qui reset le user (null) et navigate vers la page principale
+  const handleLogout = () => {
+    setUser(null);
+    navigate("/street-arts");
+  };
   return (
     <header className="header">
       <Link to="/" className="logo">
         <img src={logo} alt="logo" className="logo" />
       </Link>
       <nav className="navigation">
-        <Link to="/gallery">
-          <p className="button">Galerie</p>
+        <Link to="/gallery" className="button">
+          Galerie
         </Link>
-        <Link to="/login">
-          <p className="button">Connexion</p>
+        {!user &&
+          <Link to="/login" className="button">
+            Connexion
+          </Link>}
+        <Link to="/register" className="button">
+          Inscription
         </Link>
-        <Link to="/register">
-          <p className="button">Inscription</p>
-        </Link>
-       <div>
-          <p className="button">test</p>
-        </div>
+        {user &&
+          <button type="button" className="button" onClick={handleLogout}>
+            Déconnexion
+          </button>}
       </nav>
     </header>
   );
