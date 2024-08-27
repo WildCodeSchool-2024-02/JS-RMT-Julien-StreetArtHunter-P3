048bcb07e6b157b58dcb77704479275429b931c2
Megane <megane.loisele@gmail.com>
2024-08-27 12:19:18 +0200

    mise en place du modal Add pour ajouter un streetArt

--- client/src/pages/adminInterface/StreetArt.jsx
+++ client/src/pages/adminInterface/StreetArt.jsx
@@ -9,52 +9,134 @@ import "../../styles/reactModal.css";
 Modal.setAppElement("#root");
 
 function StreetArt() {
-  const [streetArts, setstreetArts] = useState([]);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [selectedStreetartId, setSelectedStreetartId] = useState(null);
+  const [streetArts, setStreetArts] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [artistes, setArtistes] = useState([]);
+  const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
+  const [addModalIsOpen, setAddModalIsOpen] = useState(false);
+  const [selectedStreetArtId, setSelectedStreetArtId] = useState(null);
+  const [newStreetArt, setNewStreetArt] = useState({
+    title: "",
+    description: "",
+    geolocation_x: "",
+    geolocation_y: "",
+    image_url: "",
+    image_alt: "",
+    points: "",
+    created_at: "",
+    city_id: "", // Ajout d'une propriété city_id
+    artist_id: "", // Ajout d'une propriété artist_id
+  });
 
+  // Fetch des street arts
   useEffect(() => {
     connexion
       .get("api/streetarts")
       .then((response) => {
-        setstreetArts(response.data);
+        setStreetArts(response.data);
       })
       .catch((error) => {
-        console.error("There was an error fetching the users!", error);
+        console.error("There was an error fetching the street arts!", error);
       });
   }, []);
 
-  const openModal = (userId) => {
-    setSelectedStreetartId(userId);
-    setModalIsOpen(true);
+  useEffect(() => {
+    connexion
+      .get("api/cities")
+      .then((response) => {
+        setCities(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the cities!", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    connexion
+      .get("api/artists")
+      .then((response) => {
+        setArtistes(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the artist!", error);
+      });
+  }, []);
+
+  const openDeleteModal = (streetArtId) => {
+    setSelectedStreetArtId(streetArtId);
+    setDeleteModalIsOpen(true);
   };
 
-  const closeModal = () => {
-    setSelectedStreetartId(null);
-    setModalIsOpen(false);
+  const closeDeleteModal = () => {
+    setSelectedStreetArtId(null);
+    setDeleteModalIsOpen(false);
+  };
+
+  const openAddModal = () => {
+    setAddModalIsOpen(true);
+  };
+
+  const closeAddModal = () => {
+    setAddModalIsOpen(false);
+    setNewStreetArt({
+      title: "",
+      description: "",
+      geolocation_x: "",
+      geolocation_y: "",
+      image_url: "",
+      image_alt: "",
+      points: "",
+      created_at: "",
+      city_id: "",
+      artist_id: "",
+    });
   };
 
   const handleDelete = () => {
-    if (selectedStreetartId !== null) {
+    if (selectedStreetArtId !== null) {
       connexion
-        .delete(`api/streetarts/${selectedStreetartId}`)
+        .delete(`api/streetarts/${selectedStreetArtId}`)
         .then(() => {
-          setstreetArts(
+          setStreetArts(
             streetArts.filter(
-              (streetart) => streetart.id !== selectedStreetartId
+              (streetArt) => streetArt.id !== selectedStreetArtId
             )
           );
-          closeModal();
+          closeDeleteModal();
         })
         .catch((error) => {
-          console.error("There was an error deleting the user!", error);
+          console.error("There was an error deleting the street art!", error);
         });
     }
   };
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewStreetArt((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleAddSubmit = (e) => {
+    e.preventDefault();
+    connexion
+      .post("api/streetarts", newStreetArt)
+      .then((response) => {
+        setStreetArts([...streetArts, response.data]);
+        closeAddModal();
+      })
+      .catch((error) => {
+        console.error("There was an error adding the new street art!", error);
+      });
+  };
+
   return (
     <div className="admin-table-container">
-      <h1>streetArts</h1>
+      <h1>Street Arts</h1>
+      <button type="submit" onClick={openAddModal}>
+        Add Street Art
+      </button>
       <table className="admin-table">
         <thead>
           {streetArts[0] && (
@@ -66,22 +148,106 @@ function StreetArt() {
             <Rows
               data={streetArt}
               key={streetArt.id}
-              handleDelete={() => openModal(streetArt.id)}
+              handleDelete={() => openDeleteModal(streetArt.id)}
             />
           ))}
         </tbody>
       </table>
+
       <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
+        isOpen={deleteModalIsOpen}
+        onRequestClose={closeDeleteModal}
         className="ReactModal__Content"
         overlayClassName="ReactModal_Overlay"
-        contentLabel="Confirmation Modal"
+        contentLabel="Delete Confirmation Modal"
       >
         <DeleteModalConfirmation
           onDelete={handleDelete}
-          onCancel={closeModal}
+          onCancel={closeDeleteModal}
         />
+      </Modal>
+
+      <Modal
+        isOpen={addModalIsOpen}
+        onRequestClose={closeAddModal}
+        className="ReactModal__Content"
+        overlayClassName="ReactModal_Overlay"
+        contentLabel="Add Street Art Modal"
+      >
+        <h2>Add a New Street Art</h2>
+        <form onSubmit={handleAddSubmit}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={newStreetArt.title}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={newStreetArt.description}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="text"
+            name="geolocation_x"
+            placeholder="Geolocation X"
+            value={newStreetArt.geolocation_x}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="text"
+            name="geolocation_y"
+            placeholder="Geolocation Y"
+            value={newStreetArt.geolocation_y}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="text"
+            name="image_url"
+            placeholder="Image URL"
+            value={newStreetArt.image_url}
+            onChange={handleInputChange}
+            required
+          />
+          <select
+            name="city_id"
+            value={newStreetArt.city_id}
+            onChange={handleInputChange}
+            required
+          >
+            <option value="">Select a city</option>
+            {cities.map((city) => (
+              <option key={city.id} value={city.id}>
+                {city.name}
+              </option>
+            ))}
+          </select>
+
+          <select
+            name="artist_id"
+            value={newStreetArt.artist_id}
+            onChange={handleInputChange}
+            required
+          >
+            <option value="">Select a artist</option>
+            {artistes.map((artist) => (
+              <option key={artist.id} value={artist.id}>
+                {artist.name}
+              </option>
+            ))}
+          </select>
+          <button type="submit">Add Street Art</button>
+          <button type="button" onClick={closeAddModal}>
+            Cancel
+          </button>
+        </form>
       </Modal>
     </div>
   );
