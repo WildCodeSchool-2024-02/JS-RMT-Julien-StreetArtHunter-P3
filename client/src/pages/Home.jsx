9885aea800dae08fdbf0484c227e3dd2929e9bbd
AnnDo33 <ann.dominique.d@gmail.com>
2024-08-28 17:40:50 +0200

    misen place resultat barre de recherche

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import Geolocation from "../components/Geolocation";
 import Carousel from "../components/Carousel";
-import SearchBar from "../components/SearchBar";
 
 import "../styles/Geolocation.css";
 
 function Home() {
   return (
     <div>
-      <SearchBar />
       <Geolocation />
       <Carousel />
     </div>
