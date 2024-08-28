f52d94c348536d37dd002fd6fc7c95c61339dd05
AnnDo33 <ann.dominique.d@gmail.com>
2024-08-28 16:01:33 +0200

    ajout SearchBar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,12 +1,29 @@
 import "../styles/searchBar.css";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (onSearch) {
+      onSearch(query);
+    }
+  };
+
   return (
-    <form className="search-form">
+    <form className="search-form" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Entrez une ville..."
         className="search-input"
+        value={query}
+        onChange={handleInputChange}
       />
       <button type="submit" className="search-button">
         Rechercher
@@ -14,5 +31,10 @@ function SearchBar() {
     </form>
   );
 }
+
+// Validation des props avec PropTypes
+SearchBar.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
